d56730d5ebcf826bde43d8a1516b01651bd47f3f
Jessey SEKULOSKI <SEK0003@wyndhamcentralsc.vic.edu.au>
2023-08-31 10:59:30 +1000

    i have changde the background colour and moved the buttons

--- Add and sav.py
+++ Add and sav.py
@@ -21,8 +21,8 @@ def total_books():
 total = 0
 root = Tk()
 
-root.geometry('500x300')
-root.configure(bg="blue")
+root.geometry('600x400')
+root.configure(bg="black")
 
 movie_code_label = Label(text="MOVIE CODE", font=('Verdana', 14))
 movie_name_label = Label(text='MOVIE NAME', font=('Verdana', 14))
@@ -32,9 +32,9 @@ movie_code_entry = Entry(font=('Verdana', 14), width=10)
 movie_name_entry = Entry(font=('Verdana', 14), width=20)
 author_entry = Entry(font=('Verdana', 14), width=20)
 
-update_button = Button(text='SAVE', font=('Verdana', 16), command = update_file)
-next_button = Button(text='NEXT', font=('Verdana', 14), command = next_item)
-total_button = Button(text='TOTAL', font=('Verdana', 14), command = total_books)
+update_button = Button(text='SAVE', font=('Verdana', 14), command = update_file)
+next_button = Button(text='NEXT', font=('Verdana', 13), command = next_item)
+total_button = Button(text='TOTAL', font=('Verdana', 13), command = total_books)
 
 movie_code_label.grid(row=0, column=0)
 movie_name_label.grid(row=2, column=0)
@@ -44,8 +44,8 @@ movie_code_entry.grid(row=0, column=1)
 movie_name_entry.grid(row=2, column=1)
 author_entry.grid(row=4, column=1)
 
-update_button.grid(row=6, column=0)
-next_button.grid(row=6, column=1)
-total_button.grid(row=6, column=2)
+update_button.grid(row=40, column=0)
+next_button.grid(row=40, column=1)
+total_button.grid(row=35, column=2)
 
 mainloop()
